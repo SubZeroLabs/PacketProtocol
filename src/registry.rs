86aa6bb90243eb8544a978700d8d3cd4088143ae
CoreyShupe <coreyshupe101@gmail.com>
2021-10-29 20:52:46 -0400

    Make types explicit.

--- src/registry.rs
+++ src/registry.rs
@@ -68,7 +68,7 @@ macro_rules! registry {
 
             paste! {
                 impl $registry_name {
-                    pub fn read_packet<T: $crate::packet::PacketAllocator>(handler: &mut impl [<$registry_name Handler>], reader: &mut impl std::io::Read) -> anyhow::Result<()> {
+                    pub fn read_packet<T: $crate::packet::PacketAllocator, H: [<$registry_name Handler>], R: std::io::Read>(handler: &mut H, reader: &mut R) -> anyhow::Result<()> {
                         let uncompressed_packet = T::decode(reader)?;
                         let (packet_id, data_cursor) = uncompressed_packet.into_packet_cursor(reader)?;
                         let lazy_handler = SimpleLazyHandle::new(data_cursor);
