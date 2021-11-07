1df63d2a75602abcab7971f478ec5b6ade5153b0
CoreyShupe <coreyshupe101@gmail.com>
2021-11-06 22:16:00 -0400

    Add disconnect play.

--- src/registry/play/client_bound.rs
+++ src/registry/play/client_bound.rs
@@ -1,3 +1,10 @@
 use crate::create_registry;
+use crate::protocol_version::MCProtocol;
+use minecraft_data_types::common::Chat;
 
-create_registry! {}
+create_registry! {
+    Disconnect {
+        reason: Chat,
+        |LocalProtocol => (MCProtocol::V1_17_1) => (0x1A);
+    }
+}
